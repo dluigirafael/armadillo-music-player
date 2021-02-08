56dca87646f8aef9b1e21db92b524b30f7220648
Rafael Duma <dluigirafael12@yahoo.com>
2021-02-08 16:15:21 +0200

    save file

--- src/fs.js
+++ src/fs.js
@@ -1,18 +1,28 @@
-import { fileOpen, directoryOpen, fileSave } from "browser-fs-access";
+import { fileOpen, directoryOpen, fileSave  } from "browser-fs-access";
 
 // Open all files in a directory,
 // recursively including subdirectories.
+
 async function fs() {
+  
   const blobsInDirectory = await directoryOpen({
     recursive: true,
   });
 
+
   // Save a file.
-  //   await fileSave(blob, {
-  //     fileName: "Untitled.png",
-  //     extensions: [".png"],
-  //   });
+
+    // blobsInDirectory.forEach( async function (blob) {
+    //     await fileSave(blob, {
+    //       fileName: blob.name,
+    //       extensions: [".png"],
+    //     });
+    //   });
+  
+
   console.log(blobsInDirectory);
 }
 
-export { fs };
+
+
+export { fs  };
