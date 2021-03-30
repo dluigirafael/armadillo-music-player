414793332bb5bcb5c0242447bb53c3584567139e
Rafael Duma <dluigirafael12@yahoo.com>
2021-03-30 09:46:59 +0300

    armadillo native prep

--- src/redux/CounterInput/counterInput.actions.js
+++
@@ -1,6 +0,0 @@
-export const updateInput = (nr) => {
-  return {
-    type: "UPDATE_INPUT",
-    payload: nr,
-  };
-};
--- src/redux/LibraryStatus/libraryStatus.actions.js
+++
@@ -1,6 +0,0 @@
-export const switchLibraryStatus = (status) => {
-  return {
-    type: "SWITCH_LIBRARY_STATUS",
-    payload: status,
-  };
-};
